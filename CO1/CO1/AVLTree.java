18b6301d0b2d94f8fc58015913a2830385e30fe7
Parinita <parinitatumu@gmail.com>
2026-06-01 23:32:06 +0530

    Create AVLTree.java

---
+++ CO1/CO1/AVLTree.java
@@ -0,0 +1,110 @@
+class AVLNode {
+    int key, height;
+    AVLNode left, right;
+
+    AVLNode(int d) {
+        key = d;
+        height = 1;
+    }
+}
+
+public class Main {
+
+    AVLNode root;
+
+    int height(AVLNode N) {
+        return (N == null) ? 0 : N.height;
+    }
+
+    int getBalance(AVLNode N) {
+        return (N == null) ? 0 :
+                height(N.left) - height(N.right);
+    }
+
+    AVLNode rightRotate(AVLNode y) {
+        AVLNode x = y.left;
+        AVLNode T2 = x.right;
+
+        x.right = y;
+        y.left = T2;
+
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+
+        return x;
+    }
+
+    AVLNode leftRotate(AVLNode x) {
+        AVLNode y = x.right;
+        AVLNode T2 = y.left;
+
+        y.left = x;
+        x.right = T2;
+
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+
+        return y;
+    }
+
+    AVLNode insert(AVLNode node, int key) {
+
+        if (node == null)
+            return new AVLNode(key);
+
+        if (key < node.key)
+            node.left = insert(node.left, key);
+        else if (key > node.key)
+            node.right = insert(node.right, key);
+        else
+            return node;
+
+        node.height = 1 + Math.max(height(node.left),
+                                   height(node.right));
+
+        int balance = getBalance(node);
+
+        // LL Rotation
+        if (balance > 1 && key < node.left.key)
+            return rightRotate(node);
+
+        // RR Rotation
+        if (balance < -1 && key > node.right.key)
+            return leftRotate(node);
+
+        // LR Rotation
+        if (balance > 1 && key > node.left.key) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+
+        // RL Rotation
+        if (balance < -1 && key < node.right.key) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+
+        return node;
+    }
+
+    void inorder(AVLNode root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.key + " ");
+            inorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Main tree = new Main();
+
+        int arr[] = {30, 20, 10};
+
+        for (int x : arr)
+            tree.root = tree.insert(tree.root, x);
+
+        System.out.print("AVL Tree Inorder Traversal: ");
+        tree.inorder(tree.root);
+    }
+}
