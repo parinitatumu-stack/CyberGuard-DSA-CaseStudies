dd9d5a6989819a42e50f4d0608ff30a2e6e99a1c
Parinita <parinitatumu@gmail.com>
2026-06-20 13:24:25 +0530

    Create Floyd.java

---
+++ CO-4/Floyd.java
@@ -0,0 +1,39 @@
+public class FloydWarshall {
+
+    final static int INF=99999;
+    final static int V=4;
+
+    public static void main(String args[]) {
+
+        int graph[][]={
+                {0,5,INF,10},
+                {INF,0,3,INF},
+                {INF,INF,0,1},
+                {INF,INF,INF,0}
+        };
+
+        int dist[][]=new int[V][V];
+
+        for(int i=0;i<V;i++)
+            for(int j=0;j<V;j++)
+                dist[i][j]=graph[i][j];
+
+        for(int k=0;k<V;k++)
+            for(int i=0;i<V;i++)
+                for(int j=0;j<V;j++)
+                    if(dist[i][k]+dist[k][j]<dist[i][j])
+                        dist[i][j]=dist[i][k]+dist[k][j];
+
+        System.out.println("Shortest Distance Matrix:");
+
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(dist[i][j]==INF)
+                    System.out.print("INF ");
+                else
+                    System.out.print(dist[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
