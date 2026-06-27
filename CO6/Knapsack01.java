39eb884475ed65f0ab6197db3c108e32047d0374
Parinita <parinitatumu@gmail.com>
2026-06-27 10:04:35 +0530

    Implement 0/1 Knapsack problem in Knapsack01.java

---
+++ CO6/Knapsack01.java
@@ -0,0 +1,41 @@
+public class Knapsack01 {
+
+    static int knapsack(int W, int wt[], int val[], int n){
+
+        int K[][] = new int[n+1][W+1];
+
+        for(int i=0;i<=n;i++){
+
+            for(int w=0;w<=W;w++){
+
+                if(i==0 || w==0)
+                    K[i][w]=0;
+
+                else if(wt[i-1]<=w)
+
+                    K[i][w]=Math.max(val[i-1]+K[i-1][w-wt[i-1]],K[i-1][w]);
+
+                else
+
+                    K[i][w]=K[i-1][w];
+            }
+        }
+
+        return K[n][W];
+    }
+
+    public static void main(String args[]){
+
+        int value[]={60,100,120};
+        int weight[]={10,20,30};
+
+        int capacity=50;
+
+        System.out.println("========= 0/1 Knapsack =========");
+
+        System.out.println("\nTruck Capacity : "+capacity+" kg");
+
+        System.out.println("Maximum Profit : "+knapsack(capacity,weight,value,value.length));
+
+    }
+}
